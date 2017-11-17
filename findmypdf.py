ebb85be747018d68449c0a3c826e22a7ce44dda1
Loock Underwood <30809756+LoockUnderwood@users.noreply.github.com>
2017-11-17 11:25:05 -0200

    FINDmyPDF.py
    Program which searches for your "lost" PDF on Google, it speeds up your search.

--- findmypdf.py
+++ findmypdf.py
@@ -1,29 +1,39 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+
 import webbrowser
 import time
-print ("""
 
-  ______ _____ _   _ _____                      _____    _____    ______ 
- |  ____|_   _| \ | |  __ \                    |  __ \  |  __ \  |  ____|
- | |__    | | |  \| | |  | |  _ __ ___  _   _  | |__) | | |  | | | |__   
- |  __|   | | | . ` | |  | | | '_ ` _ \| | | | |  ___/  | |  | | |  __|  
- | |     _| |_| |\  | |__| | | | | | | | |_| | | |      | |__| | | |     
- |_|    |_____|_| \_|_____/  |_| |_| |_|\__, | |_|      |_____/  |_|     
-                                         __/ |                           
-                                        |___/                            
+print("""
 
-         """)
-time.sleep(.750)
-print ("\n1 - Search")
-print ("2 - Exit\n")
-time.sleep(.500)
-menu = int(input("Your choice: "))
+ ███████╗██╗███╗   ██╗██████╗ ███╗   ███╗██╗   ██╗██████╗ ██████╗ ███████╗
+ ██╔════╝██║████╗  ██║██╔══██╗████╗ ████║╚██╗ ██╔╝██╔══██╗██╔══██╗██╔════╝
+ █████╗  ██║██╔██╗ ██║██║  ██║██╔████╔██║ ╚████╔╝ ██████╔╝██║  ██║█████╗  
+ ██╔══╝  ██║██║╚██╗██║██║  ██║██║╚██╔╝██║  ╚██╔╝  ██╔═══╝ ██║  ██║██╔══╝  
+ ██║     ██║██║ ╚████║██████╔╝██║ ╚═╝ ██║   ██║   ██║     ██████╔╝██║     
+ ╚═╝     ╚═╝╚═╝  ╚═══╝╚═════╝ ╚═╝     ╚═╝   ╚═╝   ╚═╝     ╚═════╝ ╚═╝
 
+ X----------------------------X
+ | BY: Mateus4K / Fork: Loock |
+ X----------------------------X
+ """)
+                                                                         
+
+# Local onde o usuário irá escolher a opção
+time.sleep(.1)
+print("\n 1 - Pesquisar")
+print("\n 2 - Exit")
+menu = int(input("\n OPTION: "))
+
+# Parte o qual o programa irá colocar o nome do PDF o qual deseja
+# E o programa usará o Google como fonte de pesquisa
 if(menu==1):
     url = "https://www.google.com/search?q="
-    book = str(input("Enter the desired BOOK: "))
+    book = str(input("\n [x] INSIRA O PDF DESEJADO: "))
     url += '"' + book + '"'
     url += " filetype:pdf"
-    webbrowser.open(url)
-    print ("Search Completed!")
+    webbrowser.open(url) # parte o qual o programa manda o navegador padrão abrir a URL que já está no programa
+    print ("\n [!] PESQUISA CONCLUÍDA!!")
+    time.sleep(4)
 else:
     raise SystemExit
